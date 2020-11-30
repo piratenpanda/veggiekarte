6160ffdde8b8065b433123fba71c5b47846e2bb3
Kristjan SCHMIDT <35647502+KristjanESPERANTO@users.noreply.github.com>
2020-11-30 21:54:20 +0100

    Repair Service Worker

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,19 @@
-const CACHENAME = 'veggiekarte';
+let CACHE_NAME = 'veggiekarte';
 
-console.log(CACHENAME);
+console.log(CACHE_NAME);
 
 // List of files to cache here.
 const FILES_TO_CACHE = [
- '.'
+ 'index.html',
+ 'js/veggiemap-data.js'
 ];
 
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHENAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE)
-          .then(() => self.skipWaiting());
-    })
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting())
   );
 });
 
@@ -21,12 +21,20 @@ self.addEventListener('activate', event => {
   event.waitUntil(self.clients.claim());
 });
 
+// Service Worker Caching Strategy: Stale-While-Revalidate
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.open(CACHENAME)
-      .then(cache => cache.match(event.request, {ignoreSearch: true}))
-      .then(response => {
-      return response || fetch(event.request);
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.match(event.request, {ignoreSearch: true})
+         .then(response => {
+           var fetchPromise = fetch(event.request)
+             .then(networkResponse => {
+               cache.put(event.request, networkResponse.clone());
+             return networkResponse;
+           })
+        return response || fetchPromise;
+      })
     })
   );
 });
