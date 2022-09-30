b84eb50a049449a5e8da99879e45d28a71de1c5e
Kristjan SCHMIDT <35647502+KristjanESPERANTO@users.noreply.github.com>
2022-09-30 19:50:20 +0200

    Format with Prettier

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,34 @@
-let CACHE_NAME = 'veggiekarte';
+let CACHE_NAME = "veggiekarte";
 
 console.info(CACHE_NAME);
 
 // List of files to cache here.
-const FILES_TO_CACHE = [
-  'index.html',
-  'data/places.min.json'
-];
+const FILES_TO_CACHE = ["index.html", "data/places.min.json"];
 
-
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
       .then(self.skipWaiting())
   );
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(self.clients.claim());
 });
 
 // Service Worker Caching Strategy: Stale-While-Revalidate
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.match(event.request)
-          .then(response => {
-            var fetchPromise = fetch(event.request)
-              .then(networkResponse => {
-                cache.put(event.request, networkResponse.clone());
-                return networkResponse;
-              })
-            return response || fetchPromise;
-          })
-      })
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(event.request).then((response) => {
+        var fetchPromise = fetch(event.request).then((networkResponse) => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+        return response || fetchPromise;
+      });
+    })
   );
 });
