2c873f965038ad98395e419de93e63ac87084e5d
Kristjan SCHMIDT <35647502+KristjanESPERANTO@users.noreply.github.com>
2022-09-30 21:07:10 +0200

    Linting and prettify

--- service-worker.js
+++ service-worker.js
@@ -21,14 +21,14 @@ self.addEventListener("activate", (event) => {
 // Service Worker Caching Strategy: Stale-While-Revalidate
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.match(event.request).then((response) => {
+    caches.open(CACHE_NAME).then((cache) =>
+      cache.match(event.request).then((response) => {
         const fetchPromise = fetch(event.request).then((networkResponse) => {
           cache.put(event.request, networkResponse.clone());
           return networkResponse;
         });
         return response || fetchPromise;
-      });
-    })
+      })
+    )
   );
 });
