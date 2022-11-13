e1094ea0e39638febe63cf2ee28e104a8c496929
Kristjan SCHMIDT <35647502+KristjanESPERANTO@users.noreply.github.com>
2022-11-14 00:59:42 +0100

    switch to global coordinates

--- js/datacheck.js
+++ js/datacheck.js
@@ -35,8 +35,8 @@ function veggiemap() {
   // Map
   map = L.map("map", {
     layers: [tileOSM],
-    center: [51.42, 12.0],
-    zoom: 11,
+    center: [20, 17],
+    zoom: 3,
     worldCopyJump: true,
     zoomControl: false
   });
